7234f14fa294ca7629a66c35e6b011fa33276e2f
Vinay Shenoy <vinay@obvious.in>
2020-08-31 16:01:35 +0530

    Stop loading overdue items on the home screen

--- app/src/main/java/org/simple/clinic/home/overdue/OverdueUpdate.kt
+++ app/src/main/java/org/simple/clinic/home/overdue/OverdueUpdate.kt
@@ -12,7 +12,7 @@ class OverdueUpdate(
 
   override fun update(model: OverdueModel, event: OverdueEvent): Next<OverdueModel, OverdueEffect> {
     return when (event) {
-      is CurrentFacilityLoaded -> next(model.currentFacilityLoaded(event.facility), LoadOverdueAppointments(date, event.facility))
+      is CurrentFacilityLoaded -> next(model.currentFacilityLoaded(event.facility))
       is CallPatientClicked -> dispatch(OpenContactPatientScreen(event.patientUuid))
     }
   }
